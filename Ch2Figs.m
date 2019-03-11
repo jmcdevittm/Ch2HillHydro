78b4257dfcb95049ee6dbd25058e4796959057b2
jmcdevittm <15877282+jmcdevittm@users.noreply.github.com>
2019-03-11 10:42:13 -0400

    Added time series and limited depth axis figures

--- Ch2Figs.m
+++ Ch2Figs.m
@@ -5,16 +5,88 @@ close all
 load AllRunoffPrecip.mat %Data pulled from PrecipAndQAnalysis.m
 load AllWellData.mat %Output of HillslopeHydroAnalysis.m
 
-%% Prelim figs of WT depth and Q for T1
+%General figure properties
+myFontsize = 18;
 
-for i = 1 %8 wells in T1
+%Create index for WY2016 but extended into the previous summer
+iWY2016EX = timerange('01-Jul-2015 00:00:00','01-Oct-2016 00:00:00');
+
+%% WT depth and Q for T1; Full depth range; Linear and semilog axes
+
+for i = 1:8 %8 wells in T1
     %Synchronize runoff with well
     tempTable = synchronize(T1(:,i),allRunoffPrecip(:,2),'intersection','fillwithmissing');
     
     %Plot well depth v runoff
+    %Linear axes
     figure
-    plot(tempTable.(T1.Properties.VariableNames{i}),tempTable.runoff,'.')
+    a = scatter(tempTable.runoff(iWY2016EX),tempTable.(T1.Properties.VariableNames{i})(iWY2016EX),...
+        25,datenum(tempTable.Time(iWY2016EX)));
+    set(gca,'FontSize',myFontsize)
+    ylabel('Depth to WT (mm)','FontSize',myFontsize)
+    xlabel('Runoff (mm/hr)','FontSize',myFontsize)
+    title(T1.Properties.VariableNames{i},'FontSize',myFontsize)
+    cbha = colorbar('Direction','reverse');
+    cbha.TickLabels = cellstr(datestr(cbha.Ticks,'mmddyy'));
+    saveas(a,sprintf('%s',T1.Properties.VariableNames{i}),'epsc')
     
+    %Semilog axes
     figure
-    semilogy(tempTable.(T1.Properties.VariableNames{i}),tempTable.runoff,'.')
+    b = scatter(tempTable.runoff(iWY2016EX),tempTable.(T1.Properties.VariableNames{i})(iWY2016EX),...
+        25,datenum(tempTable.Time(iWY2016EX)));
+    set(gca,'FontSize',myFontsize,'XScale','log')
+    xlim([0.1 100])
+    ylabel('Depth to WT (mm)','FontSize',myFontsize)
+    xlabel('Runoff (mm/hr)','FontSize',myFontsize)
+    title(T1.Properties.VariableNames{i},'FontSize',myFontsize)
+    cbhb = colorbar('Direction','reverse');
+    cbhb.TickLabels = cellstr(datestr(cbhb.Ticks,'mmddyy'));
+    saveas(b,sprintf('%s_LOG',T1.Properties.VariableNames{i}),'epsc')
+    close all
 end
+
+%% WT depth and Q for T1; Common depth axis for each well nest except 1; only semilog axes
+
+depthRange = [NaN -500 -900 -600]; %Depth range to plot for well nests 2-4
+for i = 1:8 %8 wells in T1
+    %Synchronize runoff with well
+    tempTable = synchronize(T1(:,i),allRunoffPrecip(:,2),'intersection','fillwithmissing');
+    
+    %Plot well depth v runoff
+    
+    %Semilog axes
+    figure
+    b = scatter(tempTable.runoff(iWY2016EX),tempTable.(T1.Properties.VariableNames{i})(iWY2016EX),...
+        25,datenum(tempTable.Time(iWY2016EX)));
+    set(gca,'FontSize',myFontsize,'XScale','log')
+    xlim([0.1 100])
+    if i > 2 %only rescale depth range for nests 2-4
+        iNest = ceil(i/2); %Find which well nest well is in
+        ylim([depthRange(iNest) 0])
+    end
+    ylabel('Depth to WT (mm)','FontSize',myFontsize)
+    xlabel('Runoff (mm/hr)','FontSize',myFontsize)
+    title(T1.Properties.VariableNames{i},'FontSize',myFontsize)
+    cbhb = colorbar('Direction','reverse');
+    cbhb.TickLabels = cellstr(datestr(cbhb.Ticks,'mmddyy'));
+    saveas(b,sprintf('%s_LOGcommonY',T1.Properties.VariableNames{i}),'epsc')
+    close all
+end
+%% Well time series, T1
+
+for i = 1:2:7
+    
+    a = figure;
+    plot(T1.Time(iWY2016EX),T1.(T1.Properties.VariableNames{i})(iWY2016EX),'b.',... %Shallow well
+        T1.Time(iWY2016EX),T1.(T1.Properties.VariableNames{i+1})(iWY2016EX),'r.')    %Deep well
+    set(gca,'FontSize',myFontsize)
+    ylabel('Depth to WT (mm)','FontSize',myFontsize)
+    title(sprintf('Well Nest %i',(i+1)/2),'FontSize',myFontsize)
+    
+    saveas(a,sprintf('TSWellNest%i',(i+1)/2),'epsc')
+%     close all
+end
+        
+    
+    
+    
--- HillslopeHydroAnalysis.m
+++ HillslopeHydroAnalysis.m
@@ -7,69 +7,81 @@ close all
 load AllRunoffPrecip.mat %Data pulled from PrecipAndQAnalysis.m
 load AllWellData.mat %Output of cell below
 
-%% Create Timetable Array with T1 well data (this code works but is commented out since output is load as .mat above
+%% Create Timetable Array with T1 well data (this code works but is commented out since output is loaded as .mat above)
 %Find start and stop dates for inclusive time range to fit all 8 wells
-% for i = 1:8
-%     startDates(i) = CalhounData(i).data.datetime(1); %#ok<*SAGROW>
-%     endDates(i) = CalhounData(i).data.datetime(end);
-% end
-% startDate = datestr(min(startDates));
-% endDate = datestr(max(endDates));
-% 
-% %Create vector at 5 min resolution for that date range
-% timeVector = (datetime(startDate):minutes(5):datetime(endDate))';
-% 
-% %Create blank timetable to add well data into
-% T1 = NaN(length(timeVector),8);
-% T1 = array2table(T1);
-% T1 = table2timetable(T1,'RowTimes',timeVector);
-% 
-% %Create variable names that reflect each well and add to T1
-% wellNames = {'W1shal','W1deep','W2shal','W2deep','W3shal','W3deep','W4shal','W4deep'};
-% T1.Properties.VariableNames = wellNames;
-% 
-% %Add data from all 8 wells into T1
-% for i = 1:8
-%     %Extract well data into a temp timetable
-%     tempWellData = CalhounData(i).data(:,1:2);
-%     tempWellData.datetime = datetime(tempWellData.datetime,'ConvertFrom','datenum');
-%     tempWellData = table2timetable(tempWellData);
-%     
-%     %Make sure tempWellData is in even 5 min. time steps
-%     tempWellData.datetime.Second = 0;
-%     tempWellData.datetime.Minute = 5*(round(tempWellData.datetime.Minute/5));
-%     
-%     %Find intersecting dates between temp variable and T1
-%     [tempOverlap,iOverlap,~] = intersect(T1.Time,tempWellData.datetime);
-%         
-%     %Write temp variable data into overlapping indices in T1
-%     T1.(T1.Properties.VariableNames{i})(iOverlap) = tempWellData.level;
-%     
-%     %Find data gaps less than 1 hour and apply a linear interp
-%     iNotNaN = find(~isnan(T1.(T1.Properties.VariableNames{i}))); %Extract data that is not NaN
-%     notNaNDates = T1.Time(iNotNaN);
-%     
-%     diffDates = diff(notNaNDates); %Find interval between non-NaN data
-%     
-%     iStartGaps = []; iOrigStartGaps = []; iOrigEndGaps = []; iGaps = []; %#ok<NASGU>
-%     iStartGaps = find((diffDates > hours(1))); %Find starting index of gaps > 1 hr, pad with 0 so same length as iNotNaN
-%     iOrigStartGaps = iNotNaN(iStartGaps)+1; %Then convert to indices in original data for start of gap
-%     iOrigEndGaps = iNotNaN(iStartGaps+1)-1; %Get end of gap by incrementing up by one index
-%     iGaps = [iOrigStartGaps iOrigEndGaps]; %Concatenate
-%     
-% %     for j = 1:size(iGaps,1) %Check to make sure gaps are all NaN
-% %         sum(~isnan(T1.(T1.Properties.VariableNames{i})(iOrigStartGaps(j):iOrigEndGaps(j))))
-% %     end
-%     
-%     T1.(T1.Properties.VariableNames{i})(iNotNaN(1):iNotNaN(end)) = ... %Use fillmissing to linear interp within data range
-%         fillmissing(T1.(T1.Properties.VariableNames{i})(iNotNaN(1):iNotNaN(end)),'linear');
-%     
-%     for j = 1:size(iGaps,1) %Overwrite gaps with NaN
-%         T1.(T1.Properties.VariableNames{i})(iGaps(j,1):iGaps(j,2)) = NaN;
+for i = 1:8
+    startDates(i) = CalhounData(i).data.datetime(1); %#ok<*SAGROW>
+    endDates(i) = CalhounData(i).data.datetime(end);
+end
+startDate = datestr(min(startDates));
+endDate = datestr(max(endDates));
+
+%Create vector at 5 min resolution for that date range
+timeVector = (datetime(startDate):minutes(5):datetime(endDate))';
+
+%Create blank timetable to add well data into
+T1 = NaN(length(timeVector),8);
+T1 = array2table(T1);
+T1 = table2timetable(T1,'RowTimes',timeVector);
+
+%Create variable names that reflect each well and add to T1
+wellNames = {'W1shal','W1deep','W2shal','W2deep','W3shal','W3deep','W4shal','W4deep'};
+T1.Properties.VariableNames = wellNames;
+
+%Add data from all 8 wells into T1
+for i = 1:8
+    %Extract well data into a temp timetable
+    tempWellData = CalhounData(i).data(:,1:2);
+    tempWellData.datetime = datetime(tempWellData.datetime,'ConvertFrom','datenum');
+    tempWellData = table2timetable(tempWellData);
+    
+    %Make sure tempWellData is in even 5 min. time steps
+    tempWellData.datetime.Second = 0;
+    tempWellData.datetime.Minute = 5*(round(tempWellData.datetime.Minute/5));
+    
+    %Find intersecting dates between temp variable and T1
+    [tempOverlap,iOverlap,~] = intersect(T1.Time,tempWellData.datetime);
+        
+    %Write temp variable data into overlapping indices in T1
+    T1.(T1.Properties.VariableNames{i})(iOverlap) = tempWellData.level;
+    
+    %Find data gaps less than 1 hour and apply a linear interp
+    iNotNaN = find(~isnan(T1.(T1.Properties.VariableNames{i}))); %Extract data that is not NaN
+    notNaNDates = T1.Time(iNotNaN);
+    
+    diffDates = diff(notNaNDates); %Find interval between non-NaN data
+    
+    iStartGaps = []; iOrigStartGaps = []; iOrigEndGaps = []; iGaps = []; %#ok<NASGU>
+    iStartGaps = find((diffDates > hours(1))); %Find starting index of gaps > 1 hr, pad with 0 so same length as iNotNaN
+    iOrigStartGaps = iNotNaN(iStartGaps)+1; %Then convert to indices in original data for start of gap
+    iOrigEndGaps = iNotNaN(iStartGaps+1)-1; %Get end of gap by incrementing up by one index
+    iGaps = [iOrigStartGaps iOrigEndGaps]; %Concatenate
+    
+%     for j = 1:size(iGaps,1) %Check to make sure gaps are all NaN
+%         sum(~isnan(T1.(T1.Properties.VariableNames{i})(iOrigStartGaps(j):iOrigEndGaps(j))))
 %     end
-%       
-% end
-% 
-% %Save as .mat file to load later
-% save('AllWellData.mat','T1');
+    
+    T1.(T1.Properties.VariableNames{i})(iNotNaN(1):iNotNaN(end)) = ... %Use fillmissing to linear interp within data range
+        fillmissing(T1.(T1.Properties.VariableNames{i})(iNotNaN(1):iNotNaN(end)),'linear');
+    
+    for j = 1:size(iGaps,1) %Overwrite gaps with NaN
+        T1.(T1.Properties.VariableNames{i})(iGaps(j,1):iGaps(j,2)) = NaN;
+    end
+      
+end
+
+%Add precip and runoff data to T1
+T1 = synchronize(T1,allRunoffPrecip,'union','fillwithMissing');
+%Save as .mat file to load later
+save('AllWellData.mat','T1');
+
+
+%% Change in h vs Runoff
+
+%Create hourly data for well depth, runoff, and precip
+T1hourly = T1;
+T1hourly.runoff = T1hourly.runoff/12; %Convert from mm/hr to just depth of water over 5 min interval
+T1hourly.discharge = T1hourly.discharge*300; %Convert from L/s to just volume of water over 5 min interval
+T1hourly{:,1:8} = T1hourly{:,1:8}./12; %Divide well levels by 12 so that sum over hourly will = average depth 
+T1hourly = retime(T1hourly,'hourly',@sum);
 
